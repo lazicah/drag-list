3d4f4f446b11ff768fd36b4bdca2ff91a81d283c
tomwyr <tomwyr@hotmail.com>
2019-03-09 10:22:13 +0100

    Add handleless constructor

--- lib/src/DragList.dart
+++ lib/src/DragList.dart
@@ -10,18 +10,6 @@ typedef Widget DragItemBuilder<T>(BuildContext context, T item, Widget handle);
 typedef void ItemReorderCallback(int from, int to);
 
 class DragList<T> extends StatefulWidget with AxisDimen {
-  DragList({
-    @required this.items,
-    @required this.itemExtent,
-    @required this.builder,
-    this.animDuration = const Duration(milliseconds: 300),
-    this.handleAlignment = 0.0,
-    this.scrollDirection = Axis.vertical,
-    this.handleBuilder,
-    this.onItemReorder,
-  }) : assert(handleAlignment >= -1.0 && handleAlignment <= 1.0,
-            'Handle alignment has to be in bounds (-1, 1) inclusive. Passed value was: $handleAlignment.');
-
   final List<T> items;
   final double itemExtent;
   final double handleAlignment;
@@ -30,6 +18,46 @@ class DragList<T> extends StatefulWidget with AxisDimen {
   final DragItemBuilder<T> builder;
   final ItemReorderCallback onItemReorder;
   final Axis scrollDirection;
+
+  DragList({
+    @required this.items,
+    @required this.itemExtent,
+    @required this.builder,
+    Duration animDuration,
+    double handleAlignment,
+    Axis scrollDirection,
+    this.handleBuilder,
+    this.onItemReorder,
+  })  : this.animDuration = animDuration ?? const Duration(milliseconds: 300),
+        this.handleAlignment = handleAlignment ?? 0.0,
+        this.scrollDirection = scrollDirection ?? Axis.vertical {
+    assert(this.handleAlignment >= -1.0 && this.handleAlignment <= 1.0,
+        'Handle alignment has to be in bounds (-1, 1) inclusive. Passed value was: $handleAlignment.');
+  }
+
+  DragList.handleless({
+    @required List<T> items,
+    @required double itemExtent,
+    Widget Function(BuildContext, T) builder,
+    Duration animDuration,
+    double handleAlignment,
+    Axis scrollDirection,
+    ItemReorderCallback onItemReorder,
+  }) : this(
+          items: items,
+          itemExtent: itemExtent,
+          handleAlignment: handleAlignment,
+          scrollDirection: scrollDirection,
+          animDuration: animDuration,
+          onItemReorder: onItemReorder,
+          handleBuilder: (_) => Container(),
+          builder: (context, item, handle) {
+            return Stack(children: [
+              builder(context, item),
+              Positioned.fill(child: handle),
+            ]);
+          },
+        );
 
   @override
   _DragListState<T> createState() => _DragListState<T>();
